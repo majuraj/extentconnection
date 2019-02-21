73361cfb051d8f79bf7afcb32b58e59ede1cbbc4
CCOE\Rajesh.Raj <rajesh.raj@dnb.no>
2019-02-21 19:00:15 +0100

    connection change

--- connections.js
+++ connections.js
@@ -59,7 +59,7 @@ module.exports.connections = {
   extent: {
     adapter: 'sails-mongo',
     host: '10.87.11.117', // host where MongoDB is running
-    port: 443, // port where MongoDB is running
+    port: 27017, // port where MongoDB is running
     database: 'extent'
   },
 
